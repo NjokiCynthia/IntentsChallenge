7b4774d8e6e30bdc6c42889dfa0c1fc39aab47a8
CYNTHIA <njokicynthia590@gmail.com>
2020-08-20 13:42:59 +0300

    Completed intents challenge.

--- app/src/main/java/com/example/intentschallenge/Details.java
+++ app/src/main/java/com/example/intentschallenge/Details.java
@@ -1,19 +1,58 @@
 package com.example.intentschallenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Details extends AppCompatActivity implements View.OnClickListener{
+
+public class Details extends AppCompatActivity implements View.OnClickListener {
+
+    EditText et_name, et_phone, et_web, et_location;
+    ImageView emoticon, face, mood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        et_name = findViewById(R.id.et_name);
+        et_phone = findViewById(R.id.et_name);
+        et_web = findViewById(R.id.et_web);
+        et_location = findViewById(R.id.et_location);
+
+        emoticon = findViewById(R.id.emoticon);
+        face = findViewById(R.id.face);
+        mood = findViewById(R.id.mood);
     }
 
     @Override
     public void onClick(View v) {
+        if (et_location.getText().toString().trim().isEmpty() || et_web.getText().toString().trim().isEmpty() ||
+                et_phone.getText().toString().trim().isEmpty() || et_name.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter the required details", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent();
+            intent.putExtra("name", et_name.getText().toString().trim());
+            intent.putExtra("phone", et_phone.getText().toString().trim());
+            intent.putExtra("web", et_web.getText().toString().trim());
+            intent.putExtra("location", et_location.getText().toString().trim());
 
+            if (v.getId() == R.id.emoticon) {
+                intent.putExtra("mood", "happy");
+            }
+            else if (v.getId() == R.id.face) {
+                intent.putExtra("mood", "sad");
+            }
+            else {
+                intent.putExtra("mood", "good");
+
+            }
+            setResult(RESULT_OK, intent);
+            Details.this.finish();
+        }
     }
 }
--- app/src/main/java/com/example/intentschallenge/MainActivity.java
+++ app/src/main/java/com/example/intentschallenge/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.intentschallenge;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,14 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_1;
     Button btn_create;
-    ImageView ic_phone, ic_location, ic_web;
+    ImageView ic_phone, ic_location, ic_web, ic_moods;
 
     final int create_contact = 1;
+
+    String number="", name="", web="", map="";
+
+    String mood="";
 
 
     @Override
@@ -29,10 +35,12 @@ public class MainActivity extends AppCompatActivity {
         ic_phone = findViewById(R.id.ic_phone);
         ic_location = findViewById(R.id.ic_location);
         ic_web = findViewById(R.id.ic_web);
+        ic_moods = findViewById(R.id.ic_moods);
 
         ic_web.setVisibility(View.GONE);
         ic_phone.setVisibility(View.GONE);
         ic_location.setVisibility(View.GONE);
+        ic_moods.setVisibility(View.GONE);
 
 
         btn_create.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +51,33 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        ic_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(number));
+                startActivity(intent);
+            }
+        });
+
+        ic_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + web));
+                startActivity(intent);
+
+            }
+        });
+
+        ic_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + map));
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     @Override
@@ -50,6 +85,33 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == create_contact){
+            if (resultCode == RESULT_OK){
+                ic_web.setVisibility(View.VISIBLE);
+                ic_phone.setVisibility(View.VISIBLE);
+                ic_location.setVisibility(View.VISIBLE);
+                ic_moods.setVisibility(View.VISIBLE);
+
+                name = data.getStringExtra("name");
+                number = data.getStringExtra("number");
+                web = data.getStringExtra("web");
+                map = data.getStringExtra("map");
+                mood = data.getStringExtra("mood");
+
+                if (mood.equals("happy"))
+                {
+                    ic_moods.setImageResource(R.drawable.ic_emoticon);
+                }
+               else if (mood.equals("sad")){
+                    ic_moods.setImageResource(R.drawable.ic_face);
+                }
+                else {
+                    ic_moods.setImageResource(R.drawable.ic_mood);
+                }
+            }
+            else
+            {
+                Toast.makeText(this, "No data was parsed", Toast.LENGTH_SHORT).show();
+            }
 
         }
     }
